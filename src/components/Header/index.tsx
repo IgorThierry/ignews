3f3b2d20feeab1e073ab3bdc80465660ace46a8b
Igor Thierry <igorthierry15@gmail.com>
2021-05-10 10:37:34 -0300

    feat stripe

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a href="" className={styles.active}>
+          <a href="/" className={styles.active}>
             Home
           </a>
           <a href="">Posts</a>
